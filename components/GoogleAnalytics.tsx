ba99243d799f803ff93a475a29ae480735e84c24
Admin <admin@pgelephant.com>
2025-11-30 02:36:24 +0500

    update

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -6,9 +6,15 @@ interface GoogleAnalyticsProps {
     gaId?: string
 }
 
+// Default Google Analytics ID
+const DEFAULT_GA_ID = 'G-ED3JM2F0VS'
+
 export default function GoogleAnalytics({ gaId }: GoogleAnalyticsProps) {
-    // Only load if GA ID is provided
-    if (!gaId) {
+    // Use provided ID, environment variable, or default ID
+    const analyticsId = gaId || process.env.NEXT_PUBLIC_GA_ID || DEFAULT_GA_ID
+
+    // Only load if GA ID is available
+    if (!analyticsId) {
         return null
     }
 
@@ -16,7 +22,7 @@ export default function GoogleAnalytics({ gaId }: GoogleAnalyticsProps) {
         <>
             {/* Google tag (gtag.js) */}
             <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+                src={`https://www.googletagmanager.com/gtag/js?id=${analyticsId}`}
                 strategy="afterInteractive"
             />
             <Script
@@ -27,7 +33,7 @@ export default function GoogleAnalytics({ gaId }: GoogleAnalyticsProps) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gaId}');
+            gtag('config', '${analyticsId}');
           `,
                 }}
             />
